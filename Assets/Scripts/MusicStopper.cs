ee0c0f112a867c449f81225db990bafcce67cf1c
JdkzGlitchmaster <jdkz260@gmail.com>
2018-04-03 10:09:03 -0400

    Music, SFX volume, final

---
+++ Assets/Scripts/MusicStopper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicStopper : MonoBehaviour {
+
+    private MusicManager theMC;
+
+    public int newTrack;
+
+    public bool switchOnStart;
+
+	// Use this for initialization
+	void Start () {
+        theMC = FindObjectOfType<MusicManager>();
+
+        if (switchOnStart)
+        {
+            theMC.SwitchTrack(newTrack);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
